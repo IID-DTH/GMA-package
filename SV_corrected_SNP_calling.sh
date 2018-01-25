03f23e881171c725f376cc7b2b0e335d3fd16971
IID-DTH <35684287+IID-DTH@users.noreply.github.com>
2018-01-25 13:29:21 +0800

    Update SV_corrected_SNP_calling.sh

--- SV_corrected_SNP_calling.sh
+++ SV_corrected_SNP_calling.sh
@@ -1,43 +1,52 @@
 #!/bin/bash
 bam=$1;
-#gt=$2;
-ploidy=$2;
-ref=$3;
+sample=$2
+gt=$3;
+ploidy=$4;
+ref=$5;
+
+echo "./SV_corrected_SNP_calling.sh <bam file> <output_sample> <gt file> <ploidy> <reference fasta>\n";
+
 
 #print "bam:$bam\nSV_gt:$SV_gt\nploidy";
 
 #${bai}=$bam.".bai";
-if [ ! -f "${bam}.bam.bai" ]; 
+if [ ! -f "${bam}.bai" ]; 
 then 
-	samtools index ${bam}.bam;
+	samtools index ${bam};
 fi
 
 
-if [ ! -f "${bam}.raw.table" ];
+if [ ! -f "${sample}.raw.table" ];
 then
-java -jar GenomeAnalysisTK.jar -T UnifiedGenotyper -I ${bam}.bam -R ${ref} -ploidy ${ploidy} -o ${bam}.raw.vcf -dcov 100000;
-java -jar GenomeAnalysisTK.jar -T VariantsToTable -V ${bam}.raw.vcf -R ${ref} -o ${bam}.raw.table -F CHROM -F POS -F REF -F ALT -F QUAL -F AC -F AN -F DP
+java -jar AddionalTools/GenomeAnalysisTK.jar -T UnifiedGenotyper -I ${bam} -R ${ref} -ploidy ${ploidy} -o ${sample}.raw.vcf -dcov 100000;
+java -jar AddionalTools/GenomeAnalysisTK.jar -T VariantsToTable -V ${sample}.raw.vcf -R ${ref} -o ${sample}.raw.table -F CHROM -F POS -F REF -F ALT -F QUAL -F AC -F AN -F DP
 fi
 
-if [ -f "${bam}.corr.table" ]; 
+if [ -f "${sample}.corr.table" ]; 
 then 
-rm ${bam}.corr.table;
+rm ${sample}.corr.table;
 fi
 
 pl=`expr ${ploidy} - 1`
 
 for i in $( seq 1 $pl );do
 	
-	if [ ! -f "${bam}.gt.$i.vcf" ];
+	if [ ! -f "${sample}.gt.$i.vcf" ];
 	then
-		awk -v p=$i '{if($5==p){print $1":"$2+1"-"$3}}' ${bam}.gt > ${bam}.gt.$i.intervals;
-		java -jar GenomeAnalysisTK.jar -T UnifiedGenotyper -I ${bam}.bam -L ${bam}.gt.$i.intervals -R ${ref} -ploidy $i -o ${bam}.gt.$i.vcf -dcov 100000;
+		awk -v p=$i '{if($5==p){print $1":"$2+1"-"$3}}' ${gt} > ${sample}.gt.$i.intervals;
+		java -jar AddionalTools/GenomeAnalysisTK.jar -T UnifiedGenotyper -I ${bam} -L ${sample}.gt.$i.intervals -R ${ref} -ploidy $i -o ${sample}.gt.$i.vcf -dcov 100000;
 	fi	
-		java -jar GenomeAnalysisTK.jar -T VariantsToTable -V ${bam}.gt.$i.vcf -R ${ref} -o ${bam}.gt.$i.table -F CHROM -F POS -F REF -F ALT -F QUAL -F AC -F AN -F DP ;
-		cat ${bam}.gt.$i.table >> ${bam}.corr.table	
+		java -jar AddionalTools/GenomeAnalysisTK.jar -T VariantsToTable -V ${sample}.gt.$i.vcf -R ${ref} -o ${sample}.gt.$i.table -F CHROM -F POS -F REF -F ALT -F QUAL -F AC -F AN -F DP ;
+		cat ${sample}.gt.$i.table >> ${sample}.corr.table	
 done	
 
-awk -v p=${ploidy} '{if($5==p){print $1":"$2+1"-"$3}}' ${bam}.gt > ${bam}.gt.${ploidy}.intervals;
-java -jar GenomeAnalysisTK.jar  -T SelectVariants  -R ${ref} -V ${bam}.raw.vcf -L ${bam}.gt.${ploidy}.intervals  -o ${bam}.gt.${ploidy}.vcf 
-java -jar GenomeAnalysisTK.jar -T VariantsToTable -V ${bam}.gt.${ploidy}.vcf -R ${ref} -o ${bam}.gt.${ploidy}.table -F CHROM -F POS -F REF -F ALT -F QUAL -F AC -F AN -F DP ;
-cat ${bam}.gt.${ploidy}.table >> ${bam}.corr.table
+awk -v p=${ploidy} '{if($5==p){print $1":"$2+1"-"$3}}' ${gt} > ${sample}.gt.${ploidy}.intervals;
+java -jar AddionalTools/GenomeAnalysisTK.jar  -T SelectVariants  -R ${ref} -V ${sample}.raw.vcf -L ${sample}.gt.${ploidy}.intervals  -o ${sample}.gt.${ploidy}.vcf 
+java -jar AddionalTools/GenomeAnalysisTK.jar -T VariantsToTable -V ${sample}.gt.${ploidy}.vcf -R ${ref} -o ${sample}.gt.${ploidy}.table -F CHROM -F POS -F REF -F ALT -F QUAL -F AC -F AN -F DP ;
+
+cat ${sample}.gt.${ploidy}.table >> ${sample}.corr.table
+sed '/CHROM/d' ${sample}.corr.table |sort -k 1,1 -k 2,2n|sed '1iCHROM\tPOS\tREF\tALT\tQUAL\tAC\tAN\tDP' >tmp
+mv tmp ${sample}.corr.table
+
+rm ${sample}.gt.*
